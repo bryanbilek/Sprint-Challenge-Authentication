75c819361937613b2ce467472a2b7a03a660d8f6
bryanbilek <you@example.com>
2020-04-24 11:00:25 -0500

    register & login work

--- api/server.js
+++ api/server.js
@@ -1,16 +1,39 @@
 const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
+const session = require("express-session");
+const KnexSessionStore = require("connect-session-knex")(session);
 
 const authenticate = require('../auth/authenticate-middleware.js');
 const authRouter = require('../auth/auth-router.js');
 const jokesRouter = require('../jokes/jokes-router.js');
+const dbConnection = require('../database/dbConfig');
 
 const server = express();
+
+const sessionConfig = {
+    name: "SeCrEt",
+    secret: process.env.SESSION_SECRET || "scottiepippen",
+    resave: false,
+    saveUninitialized: process.env.SEND_COOKIES || true,
+    cookie: {
+      maxAge: 1000 * 60 * 60, //good for 1 hr
+      secure: process.env.USE_SECURE_COOKIES || false,
+      httpOnly: true,
+    },
+    store: new KnexSessionStore({
+      knex: dbConnection,
+      tablename: "sessions",
+      sidfieldname: "sid",
+      createtable: true,
+      clearInterval: 1000 * 60 * 60, // will remove expired sessions every hour
+    }),
+  };
 
 server.use(helmet());
 server.use(cors());
 server.use(express.json());
+server.use(session(sessionConfig));
 
 server.use('/api/auth', authRouter);
 server.use('/api/jokes', authenticate, jokesRouter);
--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,53 @@
 const router = require('express').Router();
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const Users = require('./auth-model');
+const secrets = require('../secrets');
 
 router.post('/register', (req, res) => {
-  // implement registration
+  const user = req.body;
+  const hash = bcrypt.hashSync(user.password, 8);
+  user.password = hash;
+
+  Users.add(user)
+      .then(user => {
+        console.log("REGISTER", user);
+          res.status(200).json({ message: 'Registration successful' });
+      })
+      .catch(err => {
+          res.status(500).json({ message: 'Registraition failed' });
+      });
 });
 
 router.post('/login', (req, res) => {
-  // implement login
+  const { username, password } = req.body;
+
+  Users.findBy({ username })
+      .then(([user]) => {
+        console.log("LOGIN", user);
+          if (user && bcrypt.compareSync(password, user.password)) {
+              const token = generateToken(user);
+              res.status(200).json({ message: `Welcome, ${token}!` });
+          } else {
+              res.status(401).json({ message: 'Invalid username or password' })
+          }
+      })
+      .catch(err => {
+          res.status(500).json({ message: 'Problem logging in' });
+      });
 });
+
+//token
+function generateToken(user) {
+    const payload = {
+        userId: user.id,
+        username: user.username
+    };
+    const secret = secrets.jwtSecret
+    const options = {
+        expiresIn: '1h'
+    };
+    return jwt.sign(payload, secret, options);
+};
 
 module.exports = router;
